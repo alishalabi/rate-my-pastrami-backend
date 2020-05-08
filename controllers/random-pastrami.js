a4e974fd7ec23feafe84ee820f3430f69ace787d
Ali Shalabi <ashalabi13@gmail.com>
2020-05-08 16:41:45 -0700

    Api working for random pastrami route

--- controllers/random-pastrami.js
+++ controllers/random-pastrami.js
@@ -1,5 +1,11 @@
+const ApiWrapper = require('yelp-api-wrapper')
+require('dotenv').config();
+
 module.exports = app => {
-  app.get("/random-pastrami", (req, res) => {
-    console.log(req.name)
+  app.get('/random-pastrami/:zip', (req, res) => {
+    const apiInstance = new ApiWrapper(process.env.API_KEY, "pastrami", req.params.zip)
+    apiInstance.getRandom().then((json) => {
+      res.json(json)
+    }).catch(console.log("Error"))
   })
 }
--- index.js
+++ index.js
@@ -4,8 +4,11 @@ const express = require('express')
 const app = express()
 const port = 8000
 
+// Controllers
+require('./controllers/random-pastrami.js')(app)
+
 app.get('/', (req, res) => {
-  res.json(["Hello", "World"])
+  res.json("Welcome to the Rate My Pastrami server :D")
 })
 
 app.listen(port, () => console.log(`Listening at port ${port}`) )
