4605cf47ada1ffa15dec17bf71cb63d6408139cf
Ali Shalabi <ashalabi13@gmail.com>
2020-05-09 14:11:35 -0700

    Still working on cors issue

--- index.js
+++ index.js
@@ -2,13 +2,13 @@
 
 const express = require('express')
 const app = express()
-const port = 8000
+const port = 4000
 
 // Controllers
 require('./controllers/random-pastrami.js')(app)
 
 app.get('/', (req, res) => {
-  res.json("Welcome to the Rate My Pastrami server :D")
+  res.json("Welcome to the Rate My Pastrami API :D")
 })
 
 app.listen(port, () => console.log(`Listening at port ${port}`) )
